b94f9ebfde384bcb35c360ffb30a1414a1c37b07
Elbert Christopher Larosa <elbertcl@yahoo.com>
2015-05-28 22:46:15 +0700

    changes

--- PicadillyBookStore/src/picadillybookstore/domain/Admin.java
+++ PicadillyBookStore/src/picadillybookstore/domain/Admin.java
@@ -37,11 +37,11 @@ public class Admin extends User{
     //------------------------------------
     public void display()
     {
-        System.out.println("User ID      : " + this.getUserId());
-        System.out.println("Name         : " + this.getName());
-        System.out.println("Gender       : " + this.getGender());
-        System.out.println("Username     : " + this.getUsername());
-        System.out.println("Email        : " + this.getEmail());
+        System.out.println("User ID: " + this.getUserId());
+        System.out.println("Name: " + this.getName());
+        System.out.println("Gender: " + this.getGender());
+        System.out.println("Username: " + this.getUsername());
+        System.out.println("Email: " + this.getEmail());
         System.out.println("Phone Number : " + this.getPhoneNumber());
         System.out.println("");
         System.out.println("");
